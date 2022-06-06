0703aceace72754843016fcf2b289180cd4d94ca
Diego Larrain <diego@smash.money>
2022-06-06 18:36:02 -0400

    :sparkles: Update run script with contract interaction after deploy

--- scripts/run.ts
+++ scripts/run.ts
@@ -2,10 +2,18 @@
 import hre from "hardhat";
 
 const main = async () => {
+  const [owner] = await hre.ethers.getSigners();
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
+
   console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  await waveContract.getTotalWaves();
+  const waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+  await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
